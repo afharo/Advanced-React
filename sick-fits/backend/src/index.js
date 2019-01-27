81214fcc487542de72c2f43c36c1f176c55852de
Alejandro Fernández Haro <afharo@gmail.com>
2019-01-27 20:36:06 +0000

    Day 3: Prisma + GraphQL initial setup

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,20 @@
-// let's go!
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+
+const server = createServer();
+
+// TODO: Create middleware to handle cookies (JWT)
+// TODO: Create middleware to populate current user
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+    },
+  },
+  (deets) => {
+    // eslint-disable-next-line no-console
+    console.log(`Server now running at http://localhost:${deets.port}`);
+  },
+);
